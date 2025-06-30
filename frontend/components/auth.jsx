04e050168a72af004d0196c200b2ad6703a31ddf
davodhiambo <jesseomolo@yahoo.com>
2025-06-30 11:48:06 +0300

    implement global variables in auth panel.

--- frontend/components/auth.jsx
+++ frontend/components/auth.jsx
@@ -64,33 +64,33 @@ export function AuthPanel() {
         }`}
       >
         <form className="bg-white h-full px-[50px] flex flex-col justify-center items-center text-center">
-          <h1 className="font-bold text-2xl">Sign in</h1>
+          <h1 className="font-bold text-2xl text-[var(--tertiary-text)]">Sign in</h1>
           <div className="my-5 flex space-x-3">
-            <a className="border border-gray-300 rounded-full h-10 w-10 flex justify-center items-center text-gray-600">
+            <a className="border bg-[var(--primary-accent)] border-[var(--primary-accent)] rounded-full h-10 w-10 flex justify-center items-center text-[var(--primary-background)]">
               <FaFacebookF />
             </a>
-            <a className="border border-gray-300 rounded-full h-10 w-10 flex justify-center items-center text-gray-600">
+            <a className="border border-[var(--primary-accent)] rounded-full h-10 w-10 flex justify-center items-center text-[var(--primary-background)]">
               <FaGooglePlusG />
             </a>
-            <a className="border border-gray-300 rounded-full h-10 w-10 flex justify-center items-center text-gray-600">
+            <a className="border border-[var(--primary-accent)] rounded-full h-10 w-10 flex justify-center items-center text-[var(--primary-background)]">
               <FaLinkedinIn />
             </a>
           </div>
-          <span className="text-xs mb-2">or use your account</span>
+          <span className="text-xs mb-2 text-[var(--tertiary-text)]">or use your account</span>
           <input
             type="email"
             placeholder="Email"
-            className="bg-gray-200 p-3 my-2 w-full outline-none"
+            className="bg-[var(--tertiary-background)] p-3 my-2 w-full outline-none"
           />
           <input
             type="password"
             placeholder="Password"
-            className="bg-gray-200 p-3 my-2 w-full outline-none"
+            className="bg-[var(--tertiary-background)] p-3 my-2 w-full outline-none"
           />
-          <a href="#" className="text-sm text-gray-700 mt-2 mb-4">
+          <a href="#" className="text-sm text-[var(--tertiary-text)] mt-2 mb-4">
             Forgot your password?
           </a>
-          <button className="rounded-full border border-[#010c66] bg-[#010c66] text-white text-xs font-bold py-3 px-11 uppercase hover:scale-95 transition-transform">
+          <button className="rounded-full border border-[var(--primary-background)] bg-[var(--primary-background)] text-white text-xs font-bold py-3 px-11 uppercase hover:scale-95 transition-transform">
             Sign In
           </button>
         </form>
@@ -103,7 +103,7 @@ export function AuthPanel() {
         }`}
       >
         <div
-          className={`absolute left-[-100%] w-[200%] h-full bg-gradient-to-r from-[#363a80] to-[#2a2a86] text-white flex transition-transform duration-700 ${
+          className={`absolute left-[-100%] w-[200%] h-full bg-gradient-to-r from-[#363a80] to-[var(--secondary-background)] text-white flex transition-transform duration-700 ${
             rightPanelActive ? 'translate-x-1/2' : ''
           }`}
         >
