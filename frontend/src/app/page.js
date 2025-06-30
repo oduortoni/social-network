38d04a7c8728e5fad39eb165d9a7acd3c90a5acb
davodhiambo <jesseomolo@yahoo.com>
2025-06-30 11:02:49 +0300

    fix: remove clashing background rule.

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,7 +2,7 @@ import { AuthPanel } from "../../components/auth";
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[#010c66]">
+    <div className="flex items-center justify-center min-h-screen">
       <AuthPanel />
     </div>
   );
