7ac8bd1060121b759ec34fd129c7bac9e5cdad34
jamos <amosjoel91@gmail.com>
2025-07-02 17:49:21 +0300

    feat: implement database migration function for SQLite

--- backend/pkg/db/sqlite/sqlite.go
+++ backend/pkg/db/sqlite/sqlite.go
@@ -1,1 +1,25 @@
 package sqlite
+
+import (
+	"database/sql"
+
+	migration "github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func Migrate(db *sql.DB) error {
+	driver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+	if err != nil {
+		return err
+	}
+	m, err := migration.NewWithDatabaseInstance(
+		"file://backend/pkg/db/migrations/sqlite",
+		"sqlite3",
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+	return m.Up()
+}
