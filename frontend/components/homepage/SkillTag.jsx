9a261836d5568cc7c7954db4ae657c89de3f2076
karodgers <kaunda.rodgers@gmail.com>
2025-07-21 16:22:05 +0300

    feat(homepage): add SkillTag component for displaying user skills

---
+++ frontend/components/homepage/SkillTag.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const SkillTag = ({ label }) => {
+  return <div className="bg-[#010c66] border border-[#3f3fd3]/30 px-3 py-1 rounded-lg text-xs">
+      {label}
+    </div>;
+};
+export default SkillTag;
