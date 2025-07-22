c1be622bfbe174830df1b12f51861b6d8da8c907
karodgers <kaunda.rodgers@gmail.com>
2025-07-22 12:15:09 +0300

    refactor(ui): use global styling instead of hardcoded inline style

--- frontend/components/homepage/VerifiedBadge.jsx
+++ frontend/components/homepage/VerifiedBadge.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 const VerifiedBadge = () => {
-  return <div className="bg-[#3f3fd3] rounded-full p-[2px]">
-      <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  return (
+    <div className="rounded-full p-[2px]" style={{ backgroundColor: 'var(--tertiary-text)' }}>
+      <svg width="12" height="12" viewBox="0 0 24 24" fill="none">
         <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
-    </div>;
+    </div>
+  );
 };
 export default VerifiedBadge;
