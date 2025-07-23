8015a2aea782e342d7812030358eb3f1fd93b96a
Siaka385 <teddysiakaogola@gmail.com>
2025-07-23 14:20:16 +0300

    add: a struct to represent user credintial for step one

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -8,17 +8,24 @@ import (
 
 // User represents a user in the database.
 type User struct {
-	ID              int64      `json:"id"`
-	Email           string     `json:"email"`
-	Password        string     `json:"password"`
-	FirstName       *string    `json:"first_name,omitempty"`
-	LastName        *string    `json:"last_name,omitempty"`
-	DateOfBirth     *string    `json:"date_of_birth,omitempty"`
-	Avatar          *string    `json:"avatar,omitempty"`
-	Nickname        *string    `json:"nickname,omitempty"`
-	AboutMe         *string    `json:"about_me,omitempty"`
-	IsProfilePublic bool       `json:"is_profile_public"`
-	CreatedAt       time.Time  `json:"created_at"`
+	ID              int64     `json:"id"`
+	Email           string    `json:"email"`
+	Password        string    `json:"password"`
+	FirstName       *string   `json:"first_name,omitempty"`
+	LastName        *string   `json:"last_name,omitempty"`
+	DateOfBirth     *string   `json:"date_of_birth,omitempty"`
+	Avatar          *string   `json:"avatar,omitempty"`
+	Nickname        *string   `json:"nickname,omitempty"`
+	AboutMe         *string   `json:"about_me,omitempty"`
+	IsProfilePublic bool      `json:"is_profile_public"`
+	CreatedAt       time.Time `json:"created_at"`
+}
+
+// StepOneCredintial represents the user's credentials on step One of registration.
+type StepOneCredintial struct {
+	Email           string `json:"email"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirm_password"`
 }
 
 // String returns a pretty-printed multiline JSON-style representation of the User.
