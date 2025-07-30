f6a0e18a385ba93d5f87169048bdf44d478f36f1
Siaka385 <teddysiakaogola@gmail.com>
2025-07-30 11:19:49 +0300

    feat:implement a struct to carry number of followrs/follwoing

--- backend/internal/models/follow.go
+++ backend/internal/models/follow.go
@@ -3,3 +3,8 @@ package models
 type Followee struct {
 	FolloweeId int `json:"followeeid"`
 }
+
+type FollowFollowingStat struct {
+	NumberOfFollowers int `json:"numberoffollowers"`
+	NumberOfFollowing int `json:"numberoffollowing"`
+}
